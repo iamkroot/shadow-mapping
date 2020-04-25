1e63c02cfd64987952d417204d5400ada60db3c7
iamkroot <kroot.patel@gmail.com>
2020-04-25 23:03:25 +0530

    Add Camera class

---
+++ Camera.cpp
@@ -0,0 +1,73 @@
+#include "Camera.h"
+#include "glm/gtc/matrix_transform.hpp"
+
+void Camera::translate(Camera::Direction direction) {
+    switch (direction) {
+        case Direction::FORWARD:
+            position += front * translationSensitivity;
+            break;
+        case Direction::BACKWARD:
+            position -= front * translationSensitivity;
+            break;
+        case Direction::RIGHT:
+            position += right * translationSensitivity;
+            break;
+        case Direction::LEFT:
+            position -= right * translationSensitivity;
+            break;
+        case Direction::UP:
+            position += up * translationSensitivity;
+            break;
+        case Direction::DOWN:
+            position -= up * translationSensitivity;
+            break;
+    }
+    updateVectors();
+}
+
+void Camera::updateVectors() {
+    front = glm::normalize(glm::vec3(
+            cos(glm::radians(yaw)) * cos(glm::radians(pitch)),
+            sin(glm::radians(pitch)),
+            sin(glm::radians(yaw)) * cos(glm::radians(pitch))
+    ));
+    right = glm::normalize(glm::cross(front, worldUp));
+    up = glm::normalize(glm::cross(right, front));
+}
+
+glm::mat4 Camera::lookAt() {
+    return glm::lookAt(position, position + front, up);
+}
+
+Camera::Camera(const glm::vec3 &position, const glm::vec3 &target, const glm::vec3 &worldUp) : position(position),
+                                                                                               initPosition(position),
+                                                                                               worldUp(worldUp) {
+    front = glm::normalize(target - position);
+    initYaw = yaw = glm::degrees(atan2f(front.z, front.x));
+    initPitch = pitch = glm::degrees(asinf(front.y));
+    updateVectors();
+}
+
+void Camera::reset() {
+    position = initPosition;
+    yaw = initYaw;
+    pitch = initPitch;
+    zoomFactor = 1;
+    updateVectors();
+}
+
+void Camera::rotate(int x, int y) {
+    yaw -= rotationSensitivity * (float) x;
+    pitch += rotationSensitivity * (float) y;
+    updateVectors();
+}
+
+void Camera::zoom(int y) {
+    zoomFactor -= (float) y * zoomSensitivity;
+    zoomFactor = std::max(zoomFactor, 0.05f);
+    zoomFactor = std::min(zoomFactor, 20.f);
+}
+
+GLfloat Camera::getZoom() {
+    return zoomFactor;
+}
---
+++ Camera.h
@@ -0,0 +1,75 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "common.h"
+
+class Camera {
+    GLfloat yaw;
+    GLfloat pitch;
+    GLfloat initYaw;
+    GLfloat initPitch;
+    GLfloat translationSensitivity = 1;
+    GLfloat rotationSensitivity = 0.2;
+    GLfloat zoomSensitivity = 0.1;
+    GLfloat zoomFactor = 1;
+    glm::vec3 position;
+    glm::vec3 initPosition;
+    glm::vec3 up;
+    glm::vec3 worldUp{0, 1, 0};
+    glm::vec3 front;
+    glm::vec3 right;
+
+    void updateVectors();
+
+public:
+    /**
+     * @brief Simple enum to define a direction of movement
+     */
+    enum class Direction {
+        FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN
+    };
+
+    /**
+     * @brief Camera class
+     * @param position Initial location of the camera
+     * @param target Co-ordinates of where the camera is pointing
+     * @param worldUp Up vector for world view
+     */
+    Camera(const glm::vec3 &position, const glm::vec3 &target, const glm::vec3 &worldUp = {0, 1, 0});
+
+    /**
+     * @brief Move the camera along either up, front or right axis.
+     * @param direction
+     */
+    void translate(Camera::Direction direction);
+
+    /**
+     * @brief Calculate the final MVP matrix and call gluLookAt. Call this in draw loop.
+     */
+    glm::mat4 lookAt();
+
+    /**
+     * @brief Reset the camera view back to initial parameters.
+     */
+    void reset();
+
+    /**
+     * @brief Modify the yaw and pitch of the camera.
+     * @param x Horizontal offset of the movement
+     * @param y Vertical offset of the movement
+     */
+    void rotate(int x, int y);
+
+    /**
+     * @brief Change the perspective angle.
+     * @param y Vertical offset of the movement
+     */
+    void zoom(int y);
+
+    /**
+     * @brief Get the zoom factor of the camera, to be used for gluPerspective
+     */
+    GLfloat getZoom();
+};
+
+#endif //CAMERA_H
